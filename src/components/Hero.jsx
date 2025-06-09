448c03ffd8a63a730499f88e6228f1ec381f0769
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-09 13:02:52 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,8 @@ export const Hero = () => {
   return (
     <header>
       <h1>Welcome to Our Website</h1>
-      <p>Your journey to excellence starts here. Para que funcione tailwind</p>
+      <h2>Work Management PALAS CHM Cerrejón</h2>
+      <p>Your journey to excellence starts here.</p>
 
     </header>
   )
