e227facef21e4cba05195f2ead7fa89cd86b1798
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-09 16:46:59 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,52 @@
 export const Hero = () => {
   return (
     <header>
-      <h1>Welcome to Our Website</h1>
-      <h2>Work Management PALAS CHM Cerrejón</h2>
-      <p>Your journey to excellence starts here.</p>
+import { Card } from "./card"
+export const CardsContainer = () => {
+
+  const services = [
+    {
+      id: 1,
+      title: "La mona",
+      description: "Pelicula de terror",
+      img: "",
+      color: ""
+    },
+    {
+      id: 2,
+      title: "Supervisor",
+      description: "El que supervisa",
+      img: "",
+      color: ""
+    },
+    {
+      id: 3,
+      title: "El cantante",
+      description: "Hector Lavo",
+      img: "",
+      color: ""
+    },
+    {
+      id: 4,
+      title: "El despresiable",
+      description: "Pelicula de suspenso",
+      img: "",
+      color: ""
+    },    
+  ]
+    
+  return (
+    <section>
+      {
+      services.map(service => {
+        return(
+          <Card key={service.id} />
+          )        
+        })
+      }
+    </section>
+  )
+}
 
     </header>
   )
