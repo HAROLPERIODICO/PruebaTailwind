1f219d6df96a59eff48f0ee9cc8c072cb1a11636
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-09 16:50:25 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,52 +1,6 @@
 export const Hero = () => {
   return (
     <header>
-import { Card } from "./card"
-export const CardsContainer = () => {
-
-  const services = [
-    {
-      id: 1,
-      title: "La mona",
-      description: "Pelicula de terror",
-      img: "",
-      color: ""
-    },
-    {
-      id: 2,
-      title: "Supervisor",
-      description: "El que supervisa",
-      img: "",
-      color: ""
-    },
-    {
-      id: 3,
-      title: "El cantante",
-      description: "Hector Lavo",
-      img: "",
-      color: ""
-    },
-    {
-      id: 4,
-      title: "El despresiable",
-      description: "Pelicula de suspenso",
-      img: "",
-      color: ""
-    },    
-  ]
-    
-  return (
-    <section>
-      {
-      services.map(service => {
-        return(
-          <Card key={service.id} />
-          )        
-        })
-      }
-    </section>
-  )
-}
 
     </header>
   )
