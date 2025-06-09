ad5044ea00793d2918676e0ffcacfdcce392a6ef
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-09 16:54:04 -0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,9 @@
 export const Hero = () => {
   return (
     <header>
-
+      <h1>Welcome WORK MANAGEMENT</h1>
+      <H2>cONTRATO MARC PLALAS CHM CERREJON</H2>
+      <P>eSTO SERA LA ENTRADA DEL PROCESO DE BACKLOG DE confiabilidad</P>
     </header>
   )
 }
